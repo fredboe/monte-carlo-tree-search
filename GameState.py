394826832880a13d5b3a3114fdb5baf7a383a1ea
Frederik Böcker <fredboe@outlook.com>
2020-07-12 21:07:27 +0200

    This is the first version. (added: GameState)

---
+++ GameState.py
@@ -0,0 +1,34 @@
+
+class GameState:
+
+    def __init__(self, board, player_id):
+        self.board = board
+        self.player_id = player_id
+
+    @property
+    def player_id(self):
+        return self.player_id
+
+    def possible_actions(self):
+        return [self.board.index(col) for col in self.board if col[-1]==0]#TODO
+
+    def has_actions(self):
+        return any(self.possible_actions(self.board))
+
+    def result(self, action):
+        board=self.board
+        board[action].insert(self.player_id,board[action].index(0))
+        new_player_id=self.new_player_id()
+        return GameState(board,new_player_id)
+
+    def new_player_id(self):
+        return self.player_id % 2 + 1
+
+    def is_winner(self):
+        pass
+
+    def utility(self):
+        winner = self.is_winner()
+        if not winner:
+            return None
+        return 1 if winner==self.player_id else 0
