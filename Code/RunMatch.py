e6a104cc44e5b495da223271e4624f8a92c0a15a
Frederik Böcker <fredboe@outlook.com>
2020-07-14 21:09:33 +0200

    Update RunMatch.py
    version 1 of run_match
    +
    create dictionary for games and for agents

--- Code/RunMatch.py
+++ Code/RunMatch.py
@@ -1,6 +1,18 @@
+from Game import GameStateConnect4
+
+gameDic = {"Connect 4": GameStateConnect4}
+
+
 class Match:
     pass
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
+    str_game = "Connect 4"
+    agent_player = ""
+    agent_opponent = ""
     # Crate Match Object
-    pass    
+
+
+def getGame(str_game):
+    return gameDic[str_game]
