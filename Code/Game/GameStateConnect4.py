853a6b133e9d006decb92ce405e7adc108c49fd4
Frederik Böcker <fredboe@outlook.com>
2020-07-16 16:09:59 +0200

    Update GameStateConnect4.py
    Changed Board to 7x6 and updated winner method (getter and setter)

--- Code/Game/GameStateConnect4.py
+++ Code/Game/GameStateConnect4.py
@@ -1,7 +1,7 @@
 import re
 
 STR_DIAG1 = "(1.......1.......1.......1)|(2.......2.......2.......2)"
-STR_DIAG2 = "(1.....1.....1.....1)|(1.....1.....1.....1)"
+STR_DIAG2 = "(1.....1.....1.....1)|(2.....2.....2.....2)"
 STR_ROWS = "(1......1......1......1)|(2......2......2......2)"
 STR_COLS = "(1111)|(2222)"
 
@@ -10,26 +10,29 @@ class GameState:
 
     def __init__(self, board, player_id):
         self.board = board
+        self.board2 = re.sub(" ", "", self.board)
         self.player_id = player_id
         self.win_player1 = [1, 1, 1, 1]
         self.win_player2 = [2, 2, 2, 2]
         self.actions = self.possible_actions()
+        self.winner = self.winner()
         # self.cols = deepcopy(board)
         # self.rows = list(zip(*board))
 
     def __str__(self):
         headline = "Connect 4".center(23, "-")
-        length = len(self.board)
-        cols = [self.board[cell:cell+7][::-1] for cell in range(0, length, 7)]
+        length = len(self.board2)
+        print(length)
+        cols = [self.board2[cell:cell+6][::-1] for cell in range(0, length, 6)]
         rows = list(zip(*cols))
         str_board = ""
         for row in rows:
             str_board += "  ".join(row)
             str_board += "\n"
         end = ""
-        winner = self.winner()
-        if winner:
-            end += "The winner is Player "+str(winner)
+        if self.winner:
+            #print(self.terminal_state())
+            end += "The winner is Player "+str(self.winner)
         return headline+"\n\n"+str_board+"\n"+end
 
     @property
@@ -41,15 +44,23 @@ class GameState:
         self.actions = actions
 
     @property
-    def player_id(self):
+    def get_player_id(self):
         return self.player_id
 
-    @player_id.setter
+    @get_player_id.setter
     def set_player_id(self, player_id):
         self.player_id = player_id
 
+    @property
+    def get_winner(self):
+        return self.winner
+
+    @get_winner.setter
+    def set_winner(self, winner):
+        self.winner = winner
+
     def possible_actions(self):
-        important_cells = self.board[6:][::7]
+        important_cells = self.board2[5:][::6]
         length = len(important_cells)
         return [i+1 for i in range(0, length) if important_cells[i] == '0']
 
@@ -62,6 +73,7 @@ class GameState:
         board = self.board
         col_index = (action-1)*7
         i_of_zero = board[col_index:col_index+7].index('0')
+        #print(col_index+i_of_zero)
         board = self.create_new_board(board, col_index+i_of_zero)
         new_player_id = self.new_player_id()
         return GameState(board, new_player_id)
@@ -73,16 +85,16 @@ class GameState:
         return self.player_id % 2 + 1
 
     def terminal_state(self):
-        return True if self.winner() or not self.has_actions() else False
+        return True if self.winner or not self.has_actions() else False
 
+    # winner as getter and setter
     def winner(self):
         win = re.search(STR_COLS+"|"+STR_ROWS+"|"+STR_DIAG1+"|"+STR_DIAG2, self.board)
         if win:
-            return win.group()[0]
+            return int(win.group()[0])
         return 0
 
-    def utility(self):
-        winner = self.winner()
-        if not winner:
-            return None
-        return 1 if winner == self.player_id else 0
+    def utility2(self, player):
+        if not self.winner:
+            return 0
+        return 5 if self.winner == player else -2
