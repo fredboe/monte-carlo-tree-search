9c26bebd906cbc928dfc9c2100561ae3d298ca40
Frederik Böcker <fredboe@outlook.com>
2020-07-16 16:10:14 +0200

    Update RunMatch.py
    updated for board 7x6

--- Code/RunMatch.py
+++ Code/RunMatch.py
@@ -1,11 +1,13 @@
 # from collections import namedtuple
 import sys
 from Game.GameStateConnect4 import GameState
+from Agents.MCTS import MCTSTree
+
 
 """Games = namedtuple("Games", "Connect4")
 games = Games(GameState)"""
 
-start_board_list = ["0" for i in range(0, 56)]
+start_board_list = ["000000 " for i in range(0, 7)]
 start_board = "".join(start_board_list)
 
 
@@ -15,20 +17,26 @@ class Match:
         self.agent_player, self.agent_opponent = agents
         self.start_board = start_board
         self.initialGameState = GameState(start_board, 1)
+        # test phase
+        self.agent = MCTSTree(self.initialGameState)
         self.run_Match(self.initialGameState)
 
-    def get_action_from_agent(self):
-        pass
+    def get_action_from_agent(self, GameState):
+        return MCTSTree(GameState).runMCTS()
 
     def run_Match(self, GameState):
         while not GameState.terminal_state():
-            action = self.get_action_from_realWorld()
+            if GameState.player_id == 1:
+                action = self.get_action_from_agent(GameState)
+                #action = self.get_action_from_realWorld()
+            else:
+                action = self.get_action_from_realWorld()
             if action not in GameState.actions:
                 print("\n\nSorry, but this action isn't AVAILABLE.\n\n")
                 continue
             GameState = GameState.result(action)
             print(str(GameState))
-        winner = GameState.winner()
+        winner = GameState.winner
         print("Player "+str(winner)+" has won." if winner else "Nobody won!")
 
     def get_action_from_realWorld(self):
@@ -43,7 +51,8 @@ class Match:
                     print("\nSomebody gave up!")
                     sys.exit()
                 int_input = True
-            except Exception:
+            except ValueError:
+                print("Please type in a number between 1 and 8.")
                 continue
         return action
 
