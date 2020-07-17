adf9e233973b7e86446d703647bb69a21f21f51b
Frederik Böcker <68112448+fredboe@users.noreply.github.com>
2020-07-17 18:20:26 +0200

    Update AlphaBeta.py

--- Agents/AlphaBeta.py
+++ Agents/AlphaBeta.py
@@ -1,13 +1,42 @@
 class AlphaBetaAgent:
 
     def get_action(self, state, depth=5):
+        """
+        
+        Parameters:
+                    state: obj GameState
+                    depth: int
+        returns
+                action: int (result of alpha_beta_search)
+        
+        """
         return self.alpha_beta_search(state, depth)
 
     def alpha_beta_search(self, state, depth):
+        """
+        
+        Parameters:
+                    state: obj GameState
+                    depth: int
+        returns
+                action: int
+        
+        """
         alpha = float("-inf")
         beta = float("inf")
 
         def min_value(state, alpha, beta, depth):
+            """
+            
+            Parameters:
+                        state
+                        alpha: float (best score for max)
+                        beta: float (best score for min)
+                        depth: int (depth of the algorithm the tree search should go)
+            returns
+                    min value of the child nodes
+            
+            """
             if state.terminal_state():
                 return state.utility(2)
             if depth <= 0:
@@ -21,6 +50,17 @@ class AlphaBetaAgent:
             return value
 
         def max_value(state, alpha, beta, depth):
+            """
+            
+            Parameters:
+                        state
+                        alpha: float (best score for max)
+                        beta: float (best score for min)
+                        depth: int (depth of the algorithm the tree search should go)
+            returns
+                    max value of the child nodes
+                    
+            """
             if state.terminal_state():
                 return state.utility(2)
             if depth <= 0:
@@ -35,4 +75,13 @@ class AlphaBetaAgent:
         return max(state.actions, key=lambda x: min_value(state.result(x), alpha, beta, depth-1))
 
     def evaluation_function(self, state):
+        """
+        
+        Parameters:
+                    state: obj GameState
+        returns
+                number of still possible win situation
+                minus number of possible win situations of the opponent
+        
+        """
         return state.count_evaluation()
