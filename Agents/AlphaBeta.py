69fcaf19a5ad660a7e11a15fb16e21dbb137e660
Frederik Böcker <fredboe@outlook.com>
2020-07-21 19:57:50 +0200

    added player_id
    added player_id
    +
    little bug fixes

--- Agents/AlphaBeta.py
+++ Agents/AlphaBeta.py
@@ -1,33 +1,33 @@
 class AlphaBetaAgent:
 
-    def get_action(self, state, depth=5):
+    def get_action(self, state, player_id, depth=7):
         """
-        
+
         Parameters:
                     state: obj GameState
                     depth: int
         returns
                 action: int (result of alpha_beta_search)
-        
-        """
-        return self.alpha_beta_search(state, depth)
 
-    def alpha_beta_search(self, state, depth):
         """
-        
+        return self.alpha_beta_search(state, depth, player_id)
+
+    def alpha_beta_search(self, state, depth, player_id):
+        """
+
         Parameters:
                     state: obj GameState
                     depth: int
         returns
                 action: int
-        
+
         """
         alpha = float("-inf")
         beta = float("inf")
 
-        def min_value(state, alpha, beta, depth):
+        def min_value(state, alpha, beta, depth, player_id):
             """
-            
+
             Parameters:
                         state
                         alpha: float (best score for max)
@@ -35,23 +35,23 @@ class AlphaBetaAgent:
                         depth: int (depth of the algorithm the tree search should go)
             returns
                     min value of the child nodes
-            
+
             """
             if state.terminal_state():
-                return state.utility(2)
+                return state.utility(player_id)
             if depth <= 0:
                 return self.evaluation_function(state)
             value = float("inf")
             for action in state.actions:
-                value = min(value, max_value(state.result(action), alpha, beta, depth-1))
+                value = min(value, max_value(state.result(action), alpha, beta, depth-1, player_id))
                 if value <= alpha:
                     return value
                 beta = min(beta, value)
             return value
 
-        def max_value(state, alpha, beta, depth):
+        def max_value(state, alpha, beta, depth, player_id):
             """
-            
+
             Parameters:
                         state
                         alpha: float (best score for max)
@@ -59,29 +59,29 @@ class AlphaBetaAgent:
                         depth: int (depth of the algorithm the tree search should go)
             returns
                     max value of the child nodes
-                    
+
             """
             if state.terminal_state():
-                return state.utility(2)
+                return state.utility(player_id)
             if depth <= 0:
                 return self.evaluation_function(state)
             value = float("-inf")
             for action in state.actions:
-                value = max(value, min_value(state.result(action), alpha, beta, depth-1))
+                value = max(value, min_value(state.result(action), alpha, beta, depth-1, player_id))
                 if value >= beta:
                     return value
                 alpha = max(alpha, value)
             return value
-        return max(state.actions, key=lambda x: min_value(state.result(x), alpha, beta, depth-1))
+        return max(state.actions, key=lambda x: min_value(state.result(x), alpha, beta, depth-1, player_id))
 
     def evaluation_function(self, state):
         """
-        
+
         Parameters:
                     state: obj GameState
         returns
                 number of still possible win situation
                 minus number of possible win situations of the opponent
-        
+
         """
         return state.count_evaluation()
